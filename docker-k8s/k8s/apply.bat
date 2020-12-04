2ec74e1d25d4dc4964592271b530a62c4ed9562c
SparkleX <xufei.sun@sap.com>
2020-12-04 16:33:43 +0800

    dev

--- docker-k8s/k8s/apply.bat
+++ docker-k8s/k8s/apply.bat
@@ -2,7 +2,7 @@ kubectl delete deployment sunxufei-pvg
 kubectl delete deployment sunxufei-sha
 kubectl delete service sunxufei-pvg
 kubectl delete service sunxufei-sha
-kubectl delete ingress sunxufei
+kubectl delete ingress sunxufei-ingress
 
 kubectl apply -f deploy-pvg.yml
 kubectl apply -f deploy-sha.yml
